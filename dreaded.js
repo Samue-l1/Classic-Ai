554ff681dbff827cdaa115d9c658ed109a6d28be
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-04-11 04:02:53 +0300

    Update dreaded.js

--- dreaded.js
+++ dreaded.js
@@ -2037,7 +2037,8 @@ if (/balloontext/.test(command)) link = 'https://photooxy.com/logo-and-text-effe
 if (/metalliceffect/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/illuminated-metallic-effect-177.html'
 if (/embroiderytext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/create-embroidery-text-online-191.html'
 if (/flamingtext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/realistic-flaming-text-effect-online-197.html'
-client.sendMessage(m.chat, { quoted: m })
+let dreaded = await photooxy.photoOxy(link, q)
+client.sendMessage(m.chat, { image: { url: dreaded }, caption: `success` }, { quoted: m })
 }
 break;
 
